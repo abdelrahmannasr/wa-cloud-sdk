f538cb04de1538eb548f2394b8e80710e0214dce
AbdelRahman Sarhan <37974483+abdelrahmannasr@users.noreply.github.com>
2026-04-10 14:27:27 +0200

    feat(flows): export all Phase 6 types from barrel
    - Add FlowListParams, FlowListResponse, FlowGetOptions and remaining types
    - Remove US4 TODO comment

--- src/flows/index.ts
+++ src/flows/index.ts
@@ -5,13 +5,18 @@ export type {
   FlowCategory,
   FlowStatus,
   FlowValidationError,
+  FlowListParams,
+  FlowListResponse,
+  FlowGetOptions,
   CreateFlowRequest,
   CreateFlowResponse,
+  UpdateFlowMetadataRequest,
+  UpdateFlowAssetsRequest,
+  UpdateFlowAssetsResponse,
+  FlowDeleteResponse,
   FlowPublishResponse,
+  FlowDeprecateResponse,
+  FlowPreviewResponse,
 } from './types.js';
 
 export { MAX_FLOW_NAME_LENGTH, MAX_FLOW_CATEGORIES, MAX_FLOW_JSON_BYTES } from './types.js';
-
-// TODO (US4): export FlowListParams, FlowListResponse, FlowGetOptions,
-// UpdateFlowMetadataRequest, UpdateFlowAssetsRequest, UpdateFlowAssetsResponse,
-// FlowDeleteResponse, FlowDeprecateResponse, FlowPreviewResponse
